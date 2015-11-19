6f006e7034a73383d851dd2e401e9edd72f8ba9e
Derek Benson <derek.benson@tufts.edu>
2015-11-19 02:17:12 +0000

    Removed old code for lab8

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 var checkins = require('./routes/checkins');
 
 var app = express();
@@ -21,7 +20,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/', checkins);
 
 // catch 404 and forward to error handler
